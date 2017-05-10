eb1832d1f543e9b553a131ce362f906fe6f58cea
Bridget Wellen <bridgetwellen@hotmail.com>
2017-05-10 06:11:22 +0000

    Added requirements for assignment 1

--- kern/main/main.c
+++ kern/main/main.c
@@ -102,9 +102,11 @@ boot(void)
 	kprintf("%s", harvard_copyright);
 	kprintf("\n");
 
-	kprintf("Put-your-group-name-here's system version %s (%s #%d)\n",
+	kprintf("Norman & Bridget system version %s (%s #%d)\n",
 		GROUP_VERSION, buildconfig, buildversion);
 	kprintf("\n");
+
+        kprintf("Hello World!\n");
 
 	/* Early initialization. */
 	ram_bootstrap();
